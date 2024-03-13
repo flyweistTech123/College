1e7cecf738bd1a9a5d39fa1f177bbe1895ab15b0
flyweistTech123 <147515698+flyweistTech123@users.noreply.github.com>
2024-03-13 19:33:09 +0530

    updated

--- src/components/Internships/Internships.jsx
+++ src/components/Internships/Internships.jsx
@@ -8,6 +8,18 @@ import TopNavbar from "../TopNavbar";
 import intern from '../../Images/Internships.png'
 import { FaChevronDown } from "react-icons/fa";
 import { BsFilterLeft } from "react-icons/bs";
+import { IoMdEye } from "react-icons/io";
+import { IoTime } from "react-icons/io5";
+import { RiBuilding2Fill } from "react-icons/ri";
+import { IoLocation } from "react-icons/io5";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { FaCalendarDays } from "react-icons/fa6";
+
+
+
+
+
+import img from '../../Images/internshio.png'
 
 
 
@@ -88,6 +100,162 @@ const Internships = () => {
                     <div className="internships3">
                         <p>Category</p>
                         <FaChevronDown color="#121212" />
+                    </div>
+                </div>
+
+
+                <div className="internships5">
+                    <div className="internships6">
+                        <div className="internships7">
+                            <div className="internships8">
+                                <img src={img} alt="" />
+                                <div className="internships9">
+                                    <h6>Social Media Executive Internship</h6>
+                                    <p>One Impression...</p>
+                                </div>
+                            </div>
+
+                            <div className="internships10">
+                                <div className="internships11">
+                                    <p>#Engineering Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships12">
+                                    <h5>+3 more</h5>
+                                </div>
+                            </div>
+
+                            <div className="internships13">
+                                <div className="internships14">
+                                    <IoMdEye color="#FFFFFF" size={20} />
+                                    <p>289 Impression</p>
+                                </div>
+                                <div className="internships15">
+                                    <IoTime color="#FFFFFF" size={20} />
+                                    <p>20 Days Left</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="internships7">
+                            <div className="internships8">
+                                <img src={img} alt="" />
+                                <div className="internships9">
+                                    <h6>Social Media Executive Internship</h6>
+                                    <p>One Impression...</p>
+                                </div>
+                            </div>
+
+                            <div className="internships10">
+                                <div className="internships11">
+                                    <p>#Engineering Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships12">
+                                    <h5>+3 more</h5>
+                                </div>
+                            </div>
+
+                            <div className="internships13">
+                                <div className="internships14">
+                                    <IoMdEye color="#FFFFFF" size={20} />
+                                    <p>289 Impression</p>
+                                </div>
+                                <div className="internships15">
+                                    <IoTime color="#FFFFFF" size={20} />
+                                    <p>20 Days Left</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="internships7">
+                            <div className="internships8">
+                                <img src={img} alt="" />
+                                <div className="internships9">
+                                    <h6>Social Media Executive Internship</h6>
+                                    <p>One Impression...</p>
+                                </div>
+                            </div>
+
+                            <div className="internships10">
+                                <div className="internships11">
+                                    <p>#Engineering Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships11">
+                                    <p>#MBA Students</p>
+                                </div>
+                                <div className="internships12">
+                                    <h5>+3 more</h5>
+                                </div>
+                            </div>
+
+                            <div className="internships13">
+                                <div className="internships14">
+                                    <IoMdEye color="#FFFFFF" size={20} />
+                                    <p>289 Impression</p>
+                                </div>
+                                <div className="internships15">
+                                    <IoTime color="#FFFFFF" size={20} />
+                                    <p>20 Days Left</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="internships16">
+                        <div className="internships17">
+                            <div className="internships18">
+                                <img src={img} alt="" />
+                                <div className="internships19">
+                                    <h6>Social Media Executive Internship</h6>
+                                    <p><RiBuilding2Fill /> One Impression...</p>
+                                    <p> <IoLocation />Gurgaon</p>
+                                    <div className="internships20">
+                                        <div className="internships21">
+                                            <p>#Marketing </p>
+                                        </div>
+                                        <div className="internships21">
+                                            <p>#Engineering  </p>
+                                        </div>
+                                        <div className="internships21">
+                                            <p>#MBA</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div className="internships22">
+                                <hr />
+                            </div>
+
+                            <div className="internships23">
+                                <div className="internships24">
+                                    <div className="internships25">
+                                        <IoMdHeartEmpty  size={20} color="#121212"/>
+                                        <p>WatchList</p>
+                                    </div>
+                                    <div className="internships25">
+                                        <FaCalendarDays   size={20} color="#121212"/>
+                                        <p>+ Calendar</p>
+                                    </div>
+                                </div>
+
+                                <button>Apply Now</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
